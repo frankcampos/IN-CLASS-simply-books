6e30d2c7750809f24bca21f560b2234308b6a135
frank campos <frankcampos85@gmail.com>
2024-01-16 14:28:55 -0600

    now, display also the author details too

--- pages/author/[firebaseKey].js
+++ pages/author/[firebaseKey].js
@@ -5,13 +5,18 @@ import { viewAuthorDetails } from '../../api/mergedData';
 
 export default function ViewAuthor() {
   const [authorBooks, setAuthorBooks] = useState([]);
+  const [authorDetails, setAuthorDetails] = useState({});
   const router = useRouter();
   const { firebaseKey } = router.query;
 
   useEffect(() => {
     viewAuthorDetails(firebaseKey).then((response) => {
+      console.warn(response);
+      console.warn(response.books);
+      console.warn(response.first_name);
       if (response && Array.isArray(response.books)) {
         setAuthorBooks(response.books);
+        setAuthorDetails(response);
       } else {
         console.error('Unexpected response structure:', response);
       }
@@ -25,12 +30,12 @@ export default function ViewAuthor() {
           <img src={book.image} alt={book.title} className="card-img-top" style={{ width: '100%' }} />
           <div className="card-body text-grey">
             <h5 className="card-title">
-              {book.title} by {book.authorObject?.first_name} {book.authorObject?.last_name}
-              {book.authorObject?.favorite ? ' 🤍' : ''}
+              {book.title} by {authorDetails.first_name} {authorDetails.last_name}
+              {authorDetails.favorite ? ' 🤍' : ''}
             </h5>
             <p className="card-text">{book.description || ''}</p>
             <p>{book.sale ? `🏷️ Sale $${book.price}` : `$${book.price}`}</p>
-            <a href={`mailto:${book.authorObject?.email}`} className="btn btn-primary">
+            <a href={`mailto:${authorDetails.email}`} className="btn btn-primary">
               Contact Author
             </a>
           </div>
